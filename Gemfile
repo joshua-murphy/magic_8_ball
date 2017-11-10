0cc0ec3362b690c1b85fefc2c21d19d11f645481
Josh Murphy <josh.murphy11@gmail.com>
2017-11-10 16:38:35 -0700

    added colorize, bug fix, aesthetics

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+ruby "2.3.1"
+
+gem "colorize", "0.8.1"
--- magic_8_ball.rb
+++ magic_8_ball.rb
@@ -1,3 +1,5 @@
+require "colorize"
+
 @arr = [
   "YES!", "NO!", "MAYBE", "UNDOUBTEDLY!", "MOST CERTAINLY NOT!", 
   "WITH LUCK, IT MAY BE.", "HARD TO SAY RIGHT NOW.", "TRY AGAIN TOMORROW.",
@@ -8,7 +10,9 @@
 
 
 def start
-  puts "I am the Magic 8 Ball! What is your question?"
+  puts ""
+  puts "I am the Magic 8 Ball! What is your question?".blue
+  puts "It had better not be 'nothing'!".red
   question = gets.strip
   if question.downcase == "secrets?"
     secret_menu
@@ -20,8 +24,8 @@ end
 
 def secret_menu
   puts ""
-  puts "I've been hacked! How did you get in here?"
-  puts "You won't guess the secret combination!"
+  puts "I've been hacked! How did you get in here?".green
+  puts "You won't guess the secret combination!".green
   pass = gets.to_i
   check_password(pass)
 end
@@ -34,21 +38,21 @@ def check_password(pass)
   when 4321
     reset_questions
   else
-    puts "I told you! Get outta here!"
+    puts "You'll never guess it! Get outta here!".red
     exit(0)
   end
 end
 
 
 def add_question
-  puts "Fine. What answer would you like to add?"
+  puts "Fine. What answer would you like to add?".green
   @arr << gets.strip.capitalize
   start
 end
 
 
 def reset_questions
-  puts "All changes cleared!"
+  puts "All changes cleared!".green
   @arr = @arr_orig
   start
 end
@@ -57,13 +61,14 @@ end
 def question_check(q)
   if q.downcase == "nothing"
     puts ""
-    puts "Begone!"
+    puts "Begone!".red
     exit(0)    
   elsif q.end_with?("?") == true
     pick_interlude
   else
-    puts "Don't you know all questions end with '?' Ask again!"
-    ask
+    puts "Don't you know all questions end with '?' Ask again!".red
+    puts ""
+    start
   end
 end
 
@@ -72,9 +77,11 @@ def secret_chance
   chance = rand(20).ceil + 1
   case chance
     when 18, 19, 20
-      puts "Don't ask me about secrets."
+      puts "Don't ask me about secrets.".green
     when 1
-      puts "1234 is NOT a secret combination"
+      puts "1234 is NOT a secret combination".green
+    when 2
+      puts "Don't try 4321. It isn't a secret".green
   else
   end
 end  
@@ -84,14 +91,14 @@ def pick_interlude
   puts ""
   arr = ["Good question", "That is a tough one", "Let's see"]
   secret_chance
-  puts arr.sample + "..."
+  puts arr.sample.cyan + "...".cyan
   puts ""
   pick_response
 end
 
 
 def pick_response
-  puts "Your answer is: " + @arr.sample.capitalize
+  puts "Your answer is: ".cyan + @arr.sample.capitalize.yellow
   puts ""
   puts "========================="
 end
